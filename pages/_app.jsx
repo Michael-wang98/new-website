3095b9b05ba4b2fd9401dce101cb6aa1e817c991
Michael-Wang98 <michael.wang1134@gmail.com>
2024-11-29 00:31:02 -0500

    Remove SSRProvider from app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,11 +2,10 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import Layout from '../components/layout';
 import {Container} from 'react-bootstrap';
-import SSRProvider from 'react-bootstrap/SSRProvider'; // Needed for React-Bootstrap
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SSRProvider>
+    <>
       <Layout>
         <Container fluid className='no-padding'>
           <Component {...pageProps} />
@@ -53,7 +52,7 @@ function MyApp({ Component, pageProps }) {
           border-top: none;
         }
       `}</style>
-    </SSRProvider>
+      </>
   );
 }
 
